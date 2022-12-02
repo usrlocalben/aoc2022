7695e6ec268e445fb034f90574143a8f2ac86320
Benjamin Yates <benjamin@rqdq.com>
2022-12-02 07:59:46 -0600

    x

--- src/rqdq.aoc22/Day02.cs
+++ src/rqdq.aoc22/Day02.cs
@@ -4,44 +4,36 @@ namespace rqdq.aoc22 {
 
 class Day02 {
   public const string fileName = "02.txt";
-  const int Win = 3;
-  const int Tie = 2;
+  const int Tie = 2, Win = 3;
 
-  public
-  void Solve(ReadOnlySpan<byte> text) {
+  public void Solve(ReadOnlySpan<byte> text) {
     int p1 = 0, p2 = 0;
     while (!text.IsEmpty) {
       var line = BTU.PopLine(ref text);
-      var other = line[0] - (byte)'A' + 1;
-      var me = line[2] - (byte)'X' + 1;
-      p1 += Score(me, other);
-      p2 += Score(Move(other, me), other); }
-
+      var them = line[0] - (byte)'A' + 1;  // [1, 3]
+      var us   = line[2] - (byte)'X' + 1;  // [1, 3]
+      p1 += Score(them, us);
+      p2 += Score(them, Move(them, us)); }
     Console.WriteLine(p1);
     Console.WriteLine(p2); }
 
-  static
-  int Score(int player, int other) {
-    if (player == other) {
-      return player + 3; }
+  int Score(int them, int us) {
+    if (us == them) {
+      return us + 3; }
     else {
-      return player + (Winner(player, other) == player ? 6 : 0); }}
+      return us + (Winner(us, them) == us ? 6 : 0); }}
 
-  static
   int Winner(int a, int b) {
     if (a == b) return a;  // tie
-    if (a == 1) return b==3 ? a : b;  // r>s
-    if (a == 2) return b==1 ? a : b;  // p>r
-    if (a == 3) return b==2 ? a : b;  // s>p
-    throw new Exception("should never reach here"); }
+    if (a == 1) return b==3 ? a : b;   // r>s
+    if (a == 2) return b==1 ? a : b;   // p>r
+    /* a == 3*/ return b==2 ? a : b; } // s>p
 
-  static
-  int Move(int a, int wanted) {
-    if (wanted == Tie) return a;
-    if (a == 1) return wanted==Win ? 2 : 3;
-    if (a == 2) return wanted==Win ? 3 : 1;
-    if (a == 3) return wanted==Win ? 1 : 2;
-    throw new Exception("should never reach here"); } }
+  int Move(int them, int wanted) {
+    if (wanted == Tie) return them;
+    if (them == 1) return wanted==Win ? 2 : 3;
+    if (them == 2) return wanted==Win ? 3 : 1;
+    /* them== 3 */ return wanted==Win ? 1 : 2; } }
 
 
 }  // close package namespace
