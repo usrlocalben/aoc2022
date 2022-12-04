573a64279f8d0a7f7a4500eb971491f78464277f
Benjamin Yates <benjamin@rqdq.com>
2022-12-03 23:16:01 -0600

    04

---
+++ src/rqdq.aoc22/Day04.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using BTU = rqdq.rclt.ByteTextUtil;
+
+namespace rqdq.aoc22 {
+
+class Day04 : ISolution {
+  public void Solve(ReadOnlySpan<byte> text) {
+    long p1 = 0, p2 = 0;
+    while (!text.IsEmpty) {
+      BTU.ConsumeValue(ref text, out int a); BTU.ConsumeChar(ref text);
+      BTU.ConsumeValue(ref text, out int b); BTU.ConsumeChar(ref text);
+      BTU.ConsumeValue(ref text, out int x); BTU.ConsumeChar(ref text);
+      BTU.ConsumeValue(ref text, out int y); BTU.ConsumeSpace(ref text);
+
+      if (Contains(a, b, x, y) || Contains(x, y, a, b)) {
+        ++p1; }
+      if (!Outside(a, b, x, y)) {
+        ++p2; }}
+
+    Console.WriteLine(p1);
+    Console.WriteLine(p2); }
+
+  bool Contains(int a, int b, int x, int y) {
+    return a <= x && y <= b; }
+
+  bool Outside(int a, int b, int x, int y) {
+    return (y < a || x > b); }}
+
+
+}  // close package namespace
