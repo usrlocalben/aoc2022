12a77a1fb4a69a7171c94c0c96809120a846e802
Benjamin Yates <benjamin@rqdq.com>
2022-12-06 06:01:50 -0600

    x

--- src/rqdq.aoc22/Day06.cs
+++ src/rqdq.aoc22/Day06.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using System.Numerics;
-using BTU = rqdq.rclt.ByteTextUtil;
+﻿using System.Numerics;
 
 namespace rqdq.aoc22 {
 
@@ -9,17 +7,17 @@ class Day06 : ISolution {
   public void Solve(ReadOnlySpan<byte> text) {
     const int N1 = 4, N2 = 14;
 
-    int p1 = -1, p2 = -1;
+    int p1 = 0, p2 = 0;
     var buf1 = new uint[N1];
     var buf2 = new uint[N2];
 
-    for (int i=0; i<text.Length && (p1==-1||p2==-1); ++i) {
+    for (int i=0; i<text.Length && (p1==0||p2==0); ++i) {
       buf1[i % N1] = buf2[i % N2] = alphaBit(text[i]);
       var bits1 = buf1.Aggregate(0U, (ax, it) => ax|it);
       var bits2 = buf2.Aggregate(0U, (ax, it) => ax|it);
-      if (p1 == -1 && BitOperations.PopCount(bits1) == N1) {
+      if (p1 == 0 && BitOperations.PopCount(bits1) == N1) {
         p1 = i + 1; }
-      if (p2 == -1 && BitOperations.PopCount(bits2) == N2) {
+      if (p2 == 0 && BitOperations.PopCount(bits2) == N2) {
         p2 = i + 1; }}
 
     Console.WriteLine(p1);
