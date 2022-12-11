1bfa84764226cbc2d1647bab53b62fafead4ce14
Benjamin Yates <benjamin@rqdq.com>
2022-12-11 09:00:49 -0600

    11

--- src/rqdq.aoc22/App.cs
+++ src/rqdq.aoc22/App.cs
@@ -7,7 +7,7 @@ class App {
   static readonly Uri kAocDomain = new("https://adventofcode.com");
   const int kYearNum = 2022;
   const int kDayBegin = 1;
-  const int kDayEnd = 11;
+  const int kDayEnd = 12;
 
   static int Main(string[] argv) {
     var appDataDir = Environment.GetEnvironmentVariable("APPDATA");
@@ -117,6 +117,27 @@ class L {
       for (int n=0; n<size - count; ++n) {
         list.Add(new T()); } } }
 
-  }  // L
+  public static
+  long Pow(long a, long b) {
+    if (b == 0) return 1;
+    if (b == 1) return a;
+    if (b % 2 == 0) {
+      var tmp = Pow(a, b/2);
+      return tmp * tmp; }
+    else {
+      var tmp = Pow(a, b/2);
+      return a * tmp * tmp;}}
+
+  public static
+  long GCF(long a, long b) {
+    while (b != 0) {
+      var temp = b;
+      b = a % b;
+      a = temp; }
+    return a; }
+
+  public static
+  long LCM(long a, long b) {
+    return (a / GCF(a, b)) * b; }}
 
 }  // close package namespace
---
+++ src/rqdq.aoc22/Day11.cs
@@ -0,0 +1,113 @@
+﻿using System.Numerics;
+using BTU = rqdq.rclt.ByteTextUtil;
+using rqdq.rmlv;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace rqdq.aoc22 {
+
+class Day11 : ISolution {
+  const int NN = 8;
+
+  struct Mono {
+    public int pow, mul, add, mod, dt, df;
+    public long Eval(long n) => n * L.Pow(n, pow) * mul + add;
+    public long Dest(long n) => n % mod == 0 ? dt : df; }
+
+  Mono[] mono = new Mono[NN];
+  long[] mload = new long[NN];
+  long p2mod = 1;
+  List<long>[] inv1 = new List<long>[NN];
+  List<long>[] inv2 = new List<long>[NN];
+
+  public
+  void Solve(ReadOnlySpan<byte> t) {
+    for (int i=0; i<NN; ++i) {
+      inv1[i] = new();
+      inv2[i] = new();
+      BTU.PopLine(ref t); // monkey n:
+
+      BTU.ConsumeSpace(ref t);
+      // starting items:
+      BTU.PopWordSp(ref t); BTU.PopWordSp(ref t);
+      for (;;) {
+        BTU.ConsumeValue(ref t, out int worry);
+        inv1[i].Add(worry);
+        inv2[i].Add(worry);
+        if (t[0] == (byte)'\n') {
+          break; }
+        BTU.PopWordSp(ref t);  /* ", " */ }
+
+      BTU.ConsumeSpace(ref t);
+
+      // operation new = old
+      BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t);
+      var op = BTU.PopWordSp(ref t); // "*" or "+"
+      var data = BTU.Decode(BTU.PopWordSp(ref t));
+      mono[i].pow = 0; mono[i].mul = 1; mono[i].add = 0;
+      if (data == "old") {
+        if (op[0] != (byte)'*') {
+          throw new Exception("unexpected op with old"); }
+        mono[i].pow = 1; }
+      else if (int.TryParse(data, out int xx)) {
+        if (op[0] == (byte)'+') mono[i].add = xx;
+        else mono[i].mul = xx; }
+      else {
+        throw new Exception("cant parse int for op"); }
+        
+      BTU.ConsumeSpace(ref t);
+
+      // test divisible by
+      BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t);
+      BTU.ConsumeValue(ref t, out int divBy);
+      mono[i].mod = divBy;
+      p2mod = L.LCM(p2mod, divBy);
+
+      BTU.ConsumeSpace(ref t);
+
+      // if true throw to monkey
+      BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t);
+      BTU.ConsumeValue(ref t, out int trueDest);
+      mono[i].dt = trueDest;
+
+      BTU.ConsumeSpace(ref t);
+
+      // if false throw to monkey
+      BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t); BTU.PopWordSp(ref t);
+      BTU.ConsumeValue(ref t, out int falseDest);
+      mono[i].df = falseDest;
+
+      BTU.ConsumeSpace(ref t); }
+
+    // part 1
+    for (int r=0; r<20; ++r) {
+      for (int m=0; m<NN; ++m) {
+        foreach (var it in inv1[m]) {
+          var level = mono[m].Eval(it) / 3;
+          var dest = mono[m].Dest(level);
+          inv1[dest].Add(level); }
+        mload[m] += inv1[m].Count;
+        inv1[m].Clear(); }}
+    var p1 = mload.OrderByDescending(n => n)
+                  .Take(2)
+                  .Aggregate(1L, (ax, it) => ax*it);
+
+    // part 2
+    Array.Clear(mload);
+    for (int r=0; r<10000; ++r) {
+      for (int m=0; m<NN; ++m) {
+        foreach (var it in inv2[m]) {
+          var level = mono[m].Eval(it) % p2mod;
+          var dest = mono[m].Dest(level);
+          inv2[dest].Add(level); }
+        mload[m] += inv2[m].Count;
+        inv2[m].Clear(); }}
+    var p2 = mload.OrderByDescending(n => n)
+                  .Take(2)
+                  .Aggregate(1L, (ax, it) => ax*it);
+
+    Console.WriteLine(p1);
+    Console.WriteLine(p2); }}
+
+
+}  // close package namespace
